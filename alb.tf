3d28e86ab96d610d092f6b85147ed0d41b8bf23f
SAURAV KC <saurav.kc14@myhunter.cuny.edu>
2020-02-21 19:58:24 -0500

    Removed alb delete protection

--- alb.tf
+++ alb.tf
@@ -4,7 +4,7 @@ resource "aws_lb" "default" {
   load_balancer_type = "application"
   security_groups    = ["${aws_security_group.default_lb.id}"]
   subnets            = ["subnet-bbd8c195", "subnet-5f8c9f38"]
-  enable_deletion_protection = true
+  enable_deletion_protection = false
   tags = {
     Environment = "dev"
   }
